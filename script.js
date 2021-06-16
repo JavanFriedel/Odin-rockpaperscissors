3e28dae5cbd82688b82afbbd788c9924a713daca
JavanFriedel <javanfriedel@hotmail.com>
2021-06-15 22:26:27 -0400

    Added graphic user interface, With Keypress, scoreboard, round log, and reset functionality.

--- script.js
+++ script.js
@@ -23,64 +23,169 @@ function singleRound (playerSelection, computerSelection) {
 
     if (playerSelection == "rock") {
         if(computerSelection == "paper"){
-            return "You Lose! Paper beats Rock"
+            return `Player: ${playerSelection} || Computer: ${computerSelection} => You Lose!`
         } else if (computerSelection == "rock"){
             return "Its a tie! Please go Again"
         } else {
-            return "You win! Rock beats Scissors"
+            return `Player: ${playerSelection} || Computer: ${computerSelection} => You Win!`
         }
     } else if (playerSelection == "paper"){
         if(computerSelection == "scissors"){
-            return "You Lose! Scissors beats Paper"
+            return `Player: ${playerSelection} || Computer: ${computerSelection} => You Lose!`
         } else if (computerSelection == "paper"){
             return "Its a tie! Please go Again"
         } else {
-            return "You win! Paper beats Rock"
+            return `Player: ${playerSelection} || Computer: ${computerSelection} => You Win!`
         }
     } else if (playerSelection == "scissors") {
         if(computerSelection == "rock"){
-            return "You Lose! Rock beats Scissors"
+            return `Player: ${playerSelection} || Computer: ${computerSelection} => You Lose!`
         } else if (computerSelection == "scissors"){
             return "Its a tie! Please go Again"
         } else {
-            return "You win! Scissors beats paper"
+            return `Player: ${playerSelection} || Computer: ${computerSelection} => You Win!`
         }
     } else {
         return "Error: Please pick rock, paper or scissors"
     }
 }
 
-function game() {
+let wins = 0
+let losses = 0
 
-    let wins = 0
+let gameOver = false;
 
-    let losses = 0
+window.addEventListener('keyup', function (e) {
+    if (gameOver == true){
+        return
+    }    
     
-    for(let i = 0; i < 5; i++) {
-        let playerInput = prompt("Rock, Paper or Scissors?")
-        let outcome = singleRound(playerInput, computerPlay())
+    let choice = ''
 
-        if (outcome.includes("You win!")){
-            wins++
-        } else if (outcome.includes("You Lose!")) {
-            losses++
-        } else {
-            i--;
-        }
-
-        console.log(outcome + " Current Score: " + wins + " Wins, " + losses + " losses")
-        
-        if (wins >= 3 || losses >= 3){
-            break;
-        }
-    }
-    
-    if (wins > losses) {
-        console.log("You won a best of 5 against the computer!")
+    if (e.key === 'r' || e.key === '1'){
+        choice = 'rock'
+    } else if (e.key === 'p' || e.key === '2'){
+        choice = 'paper'
+    } else if (e.key === 's' || e.key === '3') {
+        choice = 'scissors'
     } else {
-        console.log("The computer wins the best of 5!")
-    }   
+        return
+    }
+
+    let outcome = singleRound(choice, computerPlay())
+    
+    updateScoreBoard(outcome);
+
+})
+
+const options = document.querySelectorAll('.playerOption')
+
+options.forEach(key => key.addEventListener('click', makeChoice));
+
+function makeChoice (e) {
+
+    console.log(this)
+    let choice = this.id
+
+    let outcome = singleRound(choice, computerPlay())
+    
+    updateScoreBoard(outcome);
+}
+
+function logOutcome (string) {
+    const log = document.querySelector('.log')
+    const newDiv = document.createElement('div')
+
+    newDiv.textContent = string
+    newDiv.classList.add('logEntry')
+
+    console.log(newDiv)
+
+    log.appendChild(newDiv)
 
 }
 
-game();
+function resetGame () {
+    document.querySelector('#player-score').textContent = 0
+    document.querySelector('#comp-score').textContent = 0
+    document.querySelector('.score').classList.remove('win')
+    document.querySelector('.score').classList.remove('loss')
+
+    const log = document.querySelector('.log')
+
+    while (log.lastElementChild != log.firstElementChild) {
+        log.removeChild(log.lastElementChild)
+    }
+
+    wins = 0
+    losses = 0
+
+    gameOver = false;
+
+    document.querySelector('.resetBtn').remove();
+    document.querySelector('.declareWinner').remove();
+}
+
+function createResetButton () {
+    const btn = document.createElement('btn')
+    const score = document.querySelector('.score')
+
+    btn.textContent = 'Reset'
+
+    btn.classList.add('resetBtn', 'btn-primary', 'btn', 'm-2')
+
+    btn.addEventListener('click', function () {
+        resetGame ();
+    })
+    
+    score.append(btn)
+}
+
+function declareWinner (result) {
+    const declare = document.createElement('div')
+    const scoreBoard = document.querySelector('.score')
+
+    declare.textContent = result;
+    declare.classList.add('declareWinner', 'mt-3')
+
+    scoreBoard.prepend(declare);
+
+    createResetButton();
+
+    gameOver = true;
+
+
+}
+
+function updateScoreBoard (outcome) {
+
+    if (outcome.includes("You Win!")){
+        wins++
+        logOutcome(outcome);
+
+        document.querySelector('.score').classList.add('win')
+        document.querySelector('.score').classList.remove('loss')
+
+    } else if (outcome.includes("You Lose!")) {
+        losses++
+        logOutcome(outcome);
+        document.querySelector('.score').classList.remove('win')
+        document.querySelector('.score').classList.add('loss')
+    } else {
+        logOutcome(outcome);
+        document.querySelector('.score').classList.remove('win')
+        document.querySelector('.score').classList.remove('loss')
+    }
+
+    //update scoreboard
+    document.querySelector('#player-score').textContent = wins
+    document.querySelector('#comp-score').textContent = losses
+
+    //check for win
+    if (wins >= 3){        
+        declareWinner('You Win!')        
+    } else if (losses >= 3) {        
+        declareWinner('You Lose!')        
+    }
+
+}
